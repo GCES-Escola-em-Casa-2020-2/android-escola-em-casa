dd49d31d86eac3e10bf969f4645f78106ed53df9
LGomees <lucasgomesjk@gmail.com>
2021-05-05 21:49:33 -0300

    :sparkles: add: Applying Clean Code and SOLID on MainActivity and AboutActivity files

--- app/src/main/java/org/cordova/quasar/corona/app/AboutActivity.java
+++ app/src/main/java/org/cordova/quasar/corona/app/AboutActivity.java
@@ -1,5 +1,6 @@
 package org.cordova.quasar.corona.app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,44 +10,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AboutActivity extends AppCompatActivity {
+    private String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_about);
+        int AboutActivityLayout = R.layout.activity_about;
+        setContentView(AboutActivityLayout);
 
-        BottomNavigationView navigationView = findViewById(R.id.navigation);
-        navigationView.setSelectedItemId(R.id.about);
-
-        navigationView.setOnNavigationItemSelectedListener(
-                item -> {
-                    switch (item.getItemId()) {
-                        case R.id.classroom:
-                            startActivity(new Intent(getApplicationContext(), WebviewActivity.class)
-                                    .putExtra("url", "https://classroom.google.com/a/estudante.se.df.gov.br"));
-                            overridePendingTransition(0, 0);
-                            navigationView.getMenu().getItem(0).setChecked(true);
-                            return true;
-                        case R.id.wikipedia:
-                            startActivity(new Intent(getApplicationContext(), WebviewActivity.class)
-                                    .putExtra("url", "https://pt.wikipedia.org/"));
-                            overridePendingTransition(0, 0);
-                            navigationView.getMenu().getItem(1).setChecked(true);
-                            return true;
-                        case R.id.questions:
-                            startActivity(new Intent(getApplicationContext(), QuestionsActivity.class));
-                            overridePendingTransition(0, 0);
-                            navigationView.getMenu().getItem(2).setChecked(true);
-                            return true;
-                        case R.id.about:
-                            startActivity(new Intent(getApplicationContext(), AboutActivity.class));
-                            overridePendingTransition(0, 0);
-                            navigationView.getMenu().getItem(3).setChecked(true);
-                            return true;
-                    }
-                    return false;
-                }
-        );
+        setupNavigationView();
     }
 
     public void myOnClick(View view) {
@@ -78,5 +51,62 @@ public class AboutActivity extends AppCompatActivity {
         super.onResume();
         BottomNavigationView navigationView = findViewById(R.id.navigation);
         navigationView.getMenu().getItem(3).setChecked(true);
+    }
+
+    private void setupNavigationView() {
+        int navigationId = R.id.navigation;
+        BottomNavigationView navigationView = findViewById(navigationId);
+
+        final int aboutId = R.id.about;
+        navigationView.setSelectedItemId(aboutId);
+
+        navigationView.setOnNavigationItemSelectedListener(
+                item -> {
+                    final int wikipediaId = R.id.wikipedia;
+                    int selectedItemId = item.getItemId();
+
+                    Context applicationContext = getApplicationContext();
+
+                    Intent webviewActivityIntent = new Intent(applicationContext, WebviewActivity.class);
+
+                    overridePendingTransition(0, 0);
+                    final int questionsId = R.id.questions;
+                    final int classroomId = R.id.classroom;
+                    switch (selectedItemId) {
+                        case classroomId: {
+                            String classroomUrl = "https://classroom.google.com/a/estudante.se.df.gov.br";
+                            boolean isUrlEqualsClassroomUrl = url.equals(classroomUrl);
+                            if (isUrlEqualsClassroomUrl) {
+                                return true;
+                            }
+                            Intent activityWithUrlIntent = webviewActivityIntent.putExtra("url", classroomUrl);
+                            startActivity(activityWithUrlIntent);
+                            return true;
+                        }
+                        case wikipediaId: {
+                            String wikipediaUrl = "https://pt.wikipedia.org/";
+                            boolean isUrlEqualsWikipediaUrl = url.equals(wikipediaUrl);
+                            if (isUrlEqualsWikipediaUrl) {
+                                return true;
+                            }
+                            Intent activityWithUrlIntent = webviewActivityIntent.putExtra("url", wikipediaUrl);
+                            startActivity(activityWithUrlIntent);
+                            return true;
+                        }
+                        case questionsId: {
+                            Intent questionsActivityIntent = new Intent(applicationContext, QuestionsActivity.class);
+                            startActivity(questionsActivityIntent);
+                            return true;
+                        }
+                        case aboutId: {
+                            Intent aboutActivityIntent = new Intent(applicationContext, AboutActivity.class);
+                            startActivity(aboutActivityIntent);
+                            return true;
+                        }
+                    }
+                    navigationView.getMenu().getItem(selectedItemId).setChecked(true);
+                    return false;
+                }
+        );
     }
 }
--- app/src/main/java/org/cordova/quasar/corona/app/MainActivity.java
+++ app/src/main/java/org/cordova/quasar/corona/app/MainActivity.java
@@ -9,8 +9,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(getApplicationContext(), WebviewActivity.class)
-                .putExtra("url", "https://classroom.google.com/a/estudante.se.df.gov.br"));
+
+        String classroomUrl = "https://classroom.google.com/a/estudante.se.df.gov.br";
+
+        Intent webviewActivityIntent = new Intent(getApplicationContext(), WebviewActivity.class);
+        Intent activityWithUrlIntent = webviewActivityIntent.putExtra("url", classroomUrl);
+        startActivity(activityWithUrlIntent);
+
         overridePendingTransition(0, 0);
     }
 }
