205ec960bb6770697813e16fded4d71982aca5d6
Rafael Ribeiro <rafaelflarrn@gmail.com>
2021-05-15 02:17:56 -0300

    Change variables for constants

--- app/src/main/java/org/cordova/quasar/corona/app/MainActivity.java
+++ app/src/main/java/org/cordova/quasar/corona/app/MainActivity.java
@@ -5,15 +5,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static org.cordova.quasar.corona.app.Constants.CLASS_ROOM_ESTUDANTE;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String classroomUrl = "https://classroom.google.com/a/estudante.se.df.gov.br";
-
         Intent webviewActivityIntent = new Intent(getApplicationContext(), WebviewActivity.class);
-        Intent activityWithUrlIntent = webviewActivityIntent.putExtra("url", classroomUrl);
+        Intent activityWithUrlIntent = webviewActivityIntent.putExtra("url", CLASS_ROOM_ESTUDANTE);
         startActivity(activityWithUrlIntent);
 
         overridePendingTransition(0, 0);
