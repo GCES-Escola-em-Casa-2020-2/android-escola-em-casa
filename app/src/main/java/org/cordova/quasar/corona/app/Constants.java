6103bb4ddc07954136ec063f00de56a508ba4c12
Rafael Ribeiro <rafaelflarrn@gmail.com>
2021-05-15 02:12:24 -0300

    Add Constants class

---
+++ app/src/main/java/org/cordova/quasar/corona/app/Constants.java
@@ -0,0 +1,11 @@
+package org.cordova.quasar.corona.app;
+
+public class Constants {
+    private Constants(){}
+
+    public static final String CLASS_ROOM_ESTUDANTE = "https://classroom.google.com/a/estudante.se.df.gov.br";
+    public static final String WIKIPEDIA_PT = "https://pt.wikipedia.org/";
+    public static final String COMO_ACESSAR = "https://escolaemcasa.se.df.gov.br/index.php/como-acessar/";
+    public static final String ESCOLA_EM_CASA = "https://escolaemcasa.se.df.gov.br/";
+
+}
